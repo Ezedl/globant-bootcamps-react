7f971060ba63bf48bc60a440702d7205ab53d524
Tomas De Lorenzo <ezedl.nacio@gmail.com>
2019-03-11 12:29:55 -0300

    Added buy car button

--- car-app/src/App.js
+++ car-app/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Car from './components/Car';
 
 class App extends Component {
+  state = {
+    cars : 0
+  }
+
+  onClickHandler = () => {
+    let newValue = this.state.cars + 1;
+    this.setState({
+      cars : newValue
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <Car/>
+          You have { this.state.cars } cars. 
+          <Button onClick={ this.onClickHandler } />
         </header>
       </div>
     );
   }
+}
+
+const Button = props => {
+  return <button onClick={ props.onClick }>Buy a car!</button>
 }
 
 export default App;
